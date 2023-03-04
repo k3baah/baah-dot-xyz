6ae7c6763d23f335f0501458bd9bbd1efca92c13
k3baah <75028994+k3baah@users.noreply.github.com>
2023-03-04 20:23:14 +0000

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.astro-theme-cactus.netlify.app/",
+	site: "https://www.kofibaah.com",
 	markdown: {
 		shikiConfig: {
 			theme: "dracula",
