2b593770984e7d9d3f30d91300c55c6baf895bb5
k3baah <75028994+k3baah@users.noreply.github.com>
2023-03-05 17:25:06 +0000

    edit desc

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,11 +2,11 @@ export const MENU_LINKS = [
 	{
 		title: "Home",
 		path: "/",
-	},
+	},/*
 	{
 		title: "About",
 		path: "/about",
-	},
+	},*/
 /*
 	{
 		title: "Blog",
--- src/site.config.ts
+++ src/site.config.ts
@@ -1,7 +1,7 @@
 const siteMeta = {
 	author: "Kofi Baah",
 	title: "Kofi Baah",
-	description: "My Blog",
+	description: "I'm Kofi. I like trying to understand reality and thinking about how technology will affect the future. ",
 	lang: "en-GB",
 	ogLocale: "en_GB",
 	githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
