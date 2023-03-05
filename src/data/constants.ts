36fbc5dfb5e6f66c2b45b4b9e178caa5b52d9d37
k3baah <75028994+k3baah@users.noreply.github.com>
2023-03-05 11:48:33 +0000

    change nav bar

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -7,16 +7,26 @@ export const MENU_LINKS = [
 		title: "About",
 		path: "/about",
 	},
+/*
 	{
 		title: "Blog",
 		path: "/posts",
+	}
+*/{
+		title: "Twitter",
+		path: "https://twitter.com/k3baah"
 	},
+	{
+
+		title: "Email",
+		path: "mailto:example@example.com"
+	}
 ];
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
+	twitter: "https://twitter.com/k3baah",
 	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	email: "k3baah@protonmail.com"
 };
